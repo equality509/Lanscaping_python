b4b8455f02dd5619ec8ca119e4d7ead6ef59fbb6
Eric <equality509@gmail.com>
2023-02-05 16:27:22 -0500

    landscaper: user can buy battery powered lawnmower

--- landscaper.py
+++ landscaper.py
@@ -3,6 +3,8 @@ landscaping = {"tool": 0, "money": 0}
 tools = [
 {"name": "Teeth", "profit": 1, "cost": 0},
 {"name": "Rusty Scissors", "profit": 5, "cost": 5},
-{"name": "Push Lawnmower", "profit": 25, "cost": 50},
+{"name": "Push Lawnmower", "profit": 50, "cost": 25},
+{"name": "Battery Powered Lawnmower", "profit": "cost": 250},
+
 
 ]
