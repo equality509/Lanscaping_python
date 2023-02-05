c70dae36623117fe0e35f31f2ec6e0c4a3381c45
Eric <equality509@gmail.com>
2023-02-05 16:21:47 -0500

    landscaper: user can buy push lawnmower

--- landscaper.py
+++ landscaper.py
@@ -3,4 +3,6 @@ landscaping = {"tool": 0, "money": 0}
 tools = [
 {"name": "Teeth", "profit": 1, "cost": 0},
 {"name": "Rusty Scissors", "profit": 5, "cost": 5},
+{"name": "Push Lawnmower", "profit": 25, "cost": },
+
 ]
